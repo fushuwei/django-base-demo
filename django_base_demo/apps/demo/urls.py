01ac9539485d58aa055dfad356a45c4c5991f851
fushuwei <331044761@qq.com>
2022-05-09 23:39:04 +0800

    提交demo

--- django_base_demo/apps/demo/urls.py
+++ django_base_demo/apps/demo/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = []
 router = routers.DefaultRouter()
 
-# router.register(r'', views.DemoViewSet, basename='demo')
+router.register(r'', views.DemoViewSet, basename='demo')
 
 urlpatterns += router.urls
--- django_base_demo/apps/demo/views.py
+++ django_base_demo/apps/demo/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+import logging
 
-# Create your views here.
+from django_base_demo.core.response import Response
+from django_base_demo.core.viewsets import BaseViewSet
+
+logger = logging.getLogger('django')
+
+
+class DemoViewSet(BaseViewSet):
+
+    def retrieve(self, request, *args, **kwargs):
+        return Response.success("ok")
