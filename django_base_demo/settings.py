0e91226b9f5ec9da0c0bcca4717f6d00cdd25111
fushuwei <331044761@qq.com>
2022-05-11 22:16:33 +0800

    引入Swagger功能

--- django_base_demo/settings.py
+++ django_base_demo/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_yasg',
     'django_base_demo.apps.user',
 ]
 
--- django_base_demo/urls.py
+++ django_base_demo/urls.py
@@ -17,13 +17,35 @@ from django.conf.urls import url
 from django.urls import include
 from django.views.static import serve
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="DEMO API",
+        default_version='v1',
+        description="This is a demo, Please replace it with your description",
+        terms_of_service="www.mochousoft.com",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
-    # templates路由
+    # swagger
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+    # templates
     url(r'^templates/(?P<path>.*)$', serve, {"document_root": 'templates'}),
 
-    # 配置静态资源路由
+    # 静态资源
     url(r'^static/(?P<path>.*)$', serve, {'document_root': 'static'}),
 
-    # 自定义模块路由
+    # 自定义模块
     url('user/', include('django_base_demo.apps.user.urls')),
 ]
