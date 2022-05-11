c594cb184594510f6247540b04f842b4718776ac
fushuwei <331044761@qq.com>
2022-05-11 18:03:07 +0800

    修改viewsets基类，删除默认方法体，要求所有子类使用时强制重写父类方法

--- django_base_demo/core/viewsets.py
+++ django_base_demo/core/viewsets.py
@@ -1,7 +1,5 @@
 from rest_framework.viewsets import GenericViewSet
 
-from django_base_demo.core.response import Response
-
 
 class BaseViewSet(GenericViewSet):
     """
@@ -12,88 +10,16 @@ class BaseViewSet(GenericViewSet):
     auto_serializer = False
 
     def list(self, request, *args, **kwargs):
-        """
-        查询列表
-
-        :param request:
-        :param args:
-        :param kwargs:
-        :return:
-        """
-
-        queryset = self.filter_queryset(self.get_queryset())
-
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response.success(serializer.data, message='请求成功')
+        pass
 
     def create(self, request, *args, **kwargs):
-        """
-        创建
-
-        :param request:
-        :param args:
-        :param kwargs:
-        :return:
-        """
-
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response.success(serializer.data, message='保存成功')
+        pass
 
     def retrieve(self, request, *args, **kwargs):
-        """
-        查询单个数据
-
-        :param request:
-        :param args:
-        :param kwargs:
-        :return:
-        """
-
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
-        return Response.success(serializer.data, message='请求成功')
+        pass
 
     def update(self, request, *args, **kwargs):
-        """
-        修改
-
-        :param request:
-        :param args:
-        :param kwargs:
-        :return:
-        """
-
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-
-        if getattr(instance, '_prefetched_objects_cache', None):
-            # If 'prefetch_related' has been applied to a queryset, we need to
-            # forcibly invalidate the prefetch cache on the instance.
-            instance._prefetched_objects_cache = {}
-
-        return Response.success(serializer.data, message='更新成功')
+        pass
 
     def destroy(self, request, *args, **kwargs):
-        """
-        删除
-
-        :param request:
-        :param args:
-        :param kwargs:
-        :return:
-        """
-
-        instance = self.get_object()
-        self.perform_destroy(instance)
-        return Response.success(message='删除成功')
+        pass
